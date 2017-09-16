8b11389161cdea89e7b4f5e13c51170acda73609
Mahesh <HubMahesh@users.noreply.github.com>
2017-09-16 21:46:39 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,19 +2,19 @@ importScripts("https://www.gstatic.com/firebasejs/4.3.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/4.3.1/firebase-messaging.js");
 
 var config = {
-    apiKey: "AIzaSyDjWsPHeua1_Aqeh5cVBwovHYnYONoiyy0",
-    authDomain: "webpushnotification-b7bff.firebaseapp.com",
-    databaseURL: "https://webpushnotification-b7bff.firebaseio.com",
-    projectId: "webpushnotification-b7bff",
-    storageBucket: "webpushnotification-b7bff.appspot.com",
-    messagingSenderId: "29012639818"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: ".....",
+    projectId: "....",
+    storageBucket: "...",
+    messagingSenderId: "..."
   };
   firebase.initializeApp(config);
 
   const messaging = firebase.messaging();
 
   messaging.setBackgroundMessageHandler(function(payload){
-  	const title= 'Drivoi';
+  	const title= 'Test';
   	const options={
   	 body: payload.data.status
   	}
